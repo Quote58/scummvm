daa9c3058626955db0714ee35f09acbb3bc01821
Joseph-Eugene Winzer <joewinzer@googlemail.com>
2018-06-28 13:52:28 +0200

    IMMORTAL: Adds pointers to rooms

--- engines/immortal/rooms.h
+++ engines/immortal/rooms.h
@@ -51,6 +51,65 @@ class Level {
 	static const Room _level6Room[];
 	static const Room _level7Room[];
 	static const Room _level8Room[];
+
+	// TODO: Rename rooms once reference is not needed anymore (like for doors)
+	static const Room *_room1_0;
+	static const Room *_room1_1;
+	static const Room *_room1_2;
+	static const Room *_room1_3;
+	static const Room *_room1_4;
+	static const Room *_room1_5;
+	static const Room *_room1_6;
+	static const Room *_room1_7;
+	static const Room *_room1_8;
+	static const Room *_room1_9;
+	static const Room *_room1_A;
+	static const Room *_room1_B;
+	static const Room *_room1_C;
+	static const Room *_room1_D;
+	static const Room *_room1_E;
+	static const Room *_room1_F;
+	static const Room *_room2_0;
+	static const Room *_room2_1;
+	static const Room *_room2_2;
+	static const Room *_room2_3;
+	static const Room *_room2_4;
+	static const Room *_room2_5;
+	static const Room *_room2_6;
+	static const Room *_room2_7;
+	static const Room *_room2_8;
+	static const Room *_room2_9;
+	static const Room *_room2_A;
+	static const Room *_room2_B;
+	static const Room *_room2_C;
+	static const Room *_room2_D;
+	static const Room *_room2_E;
+	static const Room *_room2_F;
+	static const Room *_room5_0;
+	static const Room *_room5_1;
+	static const Room *_room5_2;
+	static const Room *_room5_3;
+	static const Room *_room5_4;
+	static const Room *_room5_5;
+	static const Room *_room5_6;
+	static const Room *_room5_7;
+	static const Room *_room5_8;
+	static const Room *_room5_9;
+	static const Room *_room5_A;
+	static const Room *_room6_0;
+	static const Room *_room6_1;
+	static const Room *_room6_2;
+	static const Room *_room6_3;
+	// INFO: _room6_4 missing on purpose
+	static const Room *_room6_5;
+	static const Room *_room6_6;
+	static const Room *_room6_7;
+	static const Room *_room6_8;
+	static const Room *_room6_9;
+	static const Room *_room6_A;
+	static const Room *_room8_0;
+	static const Room *_room8_1;
+	static const Room *_room8_2;
 	// TODO:
 	// objects, doors, ..
 };
