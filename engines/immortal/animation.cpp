a85604ca277ba4149c6a98062992a5c6da19a87d
Joseph-Eugene Winzer <joewinzer@googlemail.com>
2018-07-10 19:53:19 +0200

    IMMORTAL: Adds remaining animation definitions

--- engines/immortal/animation.cpp
+++ engines/immortal/animation.cpp
@@ -48,24 +48,21 @@ static const int spikeSmallFrames[] = {
 	-1
 };
 static const AnimationInfo spikeSmall = {
-	kAnimationWorm, kFileLevel1T5, 2, 1, spikeSmallFrames
+	kAnimationSpikeSmall, kFileLevel3, 2, 1, spikeSmallFrames
 };
 
-// TODO: Find file that has shrinking animation
-static const int shrinkFrames[] = {
+static const int wizardShrinkFrames[] = {
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1
 };
-static const AnimationInfo shrink = {
-	// TODO: file not found for shrink animation
-	// kAnimationShrink, kfile , 9, 1, shrinkFrames
+static const AnimationInfo wizardShrink = {
+	 kAnimationWizardShrink, kFileLevel5, 12, 1, wizardShrinkFrames
 };
 
-static const int shrinkReverseFrames[] = {
+static const int wizardGrowFrames[] = {
 	8, 7, 6, 5, 4, 3, 2, 1, 0, -1
 };
-static const AnimationInfo shrinkReverse = {
-	// TODO: file not found for shrink animation
-//	kAnimationWizardGrow, kfile , 9, 1, shrinkReverseFrames
+static const AnimationInfo wizardGrow = {
+	kAnimationWizardGrow, kFileLevel5, 12, 1, wizardGrowFrames
 };
 
 
@@ -74,7 +71,6 @@ static const int attackShortFrames[] = {
 	7, 0, -1
 };
 static const AnimationInfo attackShort = {
-	// TODO: file not found for shrink animation
 	kAnimationAttackShort, (FileId)placeholder, placeholder, 1, attackShortFrames
 };
 
@@ -85,7 +81,6 @@ static const int attackLongFrames[] = {
 	10, 10, 10,  9,  8,  8,  8,  8,  9, 10, 10, 10,  9,  8,  0, -1
 };
 static const AnimationInfo attackLong = {
-	// TODO: file not found for shrink animation
 	kAnimationAttackLong, (FileId)placeholder, placeholder, 1, attackLongFrames
 };
 
@@ -93,7 +88,6 @@ static const int attackWaitFrames[] = {
 	0, 0, 0, 0, 0, 0, 0, -1
 };
 static const AnimationInfo attackWait = {
-	// TODO: file not found for shrink animation
 	kAnimationAttackWait, (FileId)placeholder, placeholder, 1, attackWaitFrames
 };
 
@@ -102,7 +96,6 @@ static const int mordamirIncineratedFrames[] = {
 	19, 19, 19, 19, 18, 17, 16, 15, 14, 13, 12,  0, -1
 };
 static const AnimationInfo mordamirIncinerated = {
-	// TODO: file not found for shrink animation
 	kAnimationMordamirIncinerated, (FileId)placeholder, placeholder, 1, mordamirIncineratedFrames
 };
 
@@ -114,13 +107,13 @@ static const AnimationInfo recoil = {
 	kAnimationRecoil, (FileId)placeholder, placeholder, 1, recoilFrames
 };
 
-static const int mordamirAppearFrames[] = {
+static const int mordamirAppearsFrames[] = {
 	12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,
 	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 	 0,  0,  0,  0,  0,  0,  0,  0,  0, -1
 };
-static const AnimationInfo mordamirAppear = {
-	kAnimationMordamirAppears, (FileId)placeholder, placeholder, 1, mordamirAppearFrames
+static const AnimationInfo mordamirAppears = {
+	kAnimationMordamirAppears, (FileId)placeholder, placeholder, 1, mordamirAppearsFrames
 };
 
 static const int mordamirLight0Frames[] = {
@@ -139,7 +132,7 @@ static const AnimationInfo mordamirLight1 = {
 	kAnimationMordamirLight1, (FileId)placeholder, placeholder, 1, mordamirLight1Frames
 };
 
-// from wizard to mordamir
+// amulet from wizard to mordamir
 static const int mordamirTractorFrames[] = {
 	28, 29, 30, 31, 31, 31, 31, 31, 31, 32, 0, -1
 };
@@ -233,6 +226,240 @@ static const int mordamirFireFrames[] = {
 };
 static const AnimationInfo mordamirFire = {
 	kAnimationMordamirFire, kFileLevel8P2, 3, 1, mordamirFireFrames
+};
+
+static const int norlacBubbles0Frames[] = {
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, -1
+};
+static const AnimationInfo norlacBubbles0 = {
+	kAnimationNorlacBubbles0, kFileLevel7P2, 3, 1, norlacBubbles0Frames
+};
+
+static const int norlacBubbles1Frames[] = {
+	15, 16, 17, 18, 19, 20, 21, 22, -1
+};
+static const AnimationInfo norlacBubbles1 = {
+	kAnimationNorlacBubbles1, kFileLevel7P2, 3, 1, norlacBubbles1Frames
+};
+
+static const int bloodSplatterLeftFrames[] = {
+	0, 1, 2, -1
+};
+static const AnimationInfo bloodSplatter = {
+	kAnimationBloodSplatterLeft, kFileGeneral2, 3, 1,bloodSplatterLeftFrames
+};
+
+static const int bloodSplatterRightFrames[] = {
+	3, 4, 5, -1
+};
+static const AnimationInfo bloodSplatterRight = {
+	kAnimationBloodSplatterRight, kFileGeneral2, 3, 1, bloodSplatterRightFrames
+};
+
+static const int wizardSparksLeftFrames[] = {
+	6, 7, 8, 9, 10, -1
+};
+static const AnimationInfo wizardSparksLeft = {
+	kAnimationWizardSparksLeft, kFileGeneral2, 3, 1, wizardSparksLeftFrames
+};
+
+static const int wizardSparksRightFrames[] = {
+	11, 12, 13, 14, 15, -1
+};
+static const AnimationInfo wizardSparksRight = {
+	kAnimationWizardSparksRight, kFileGeneral2, 3, 1, wizardSparksRightFrames
+};
+
+static const int wizardSparksCenterFrames[] = {
+	16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, -1
+};
+static const AnimationInfo wizardSparksCenter = {
+	kAnimationWizardSparksCenter, kFileGeneral2, 3, 1, wizardSparksCenterFrames
+};
+
+static const int turretBarrelLeftFrames[] = {
+	0, 1, 2, 3, 4, -1
+};
+static const AnimationInfo turretBarrelLeft = {
+	kAnimationTurretBarrelLeft, kFileLevel134, 0, 1, turretBarrelLeftFrames
+};
+
+static const int turretBarrelRightFrames[] = {
+	5, 6, 7, 8, 9, -1
+};
+static const AnimationInfo turretBarrelRight = {
+	kAnimationTurretBarrelRight, kFileLevel134, 0, 1, turretBarrelRightFrames
+};
+
+static const int turretSkullRightFrames[] = {
+	10,  11,  12,  13,  14,  -1
+};
+static const AnimationInfo turretSkullRight = {
+	kAnimationTurretSkullRight, kFileLevel134, 0, 1, turretSkullRightFrames
+};
+
+static const int turretSkullLeftFrames[] = {
+	15,  16,  17,  18,  19,  -1
+};
+static const AnimationInfo turretSkullLeft = {
+	kAnimationTurretSkullLeft, kFileLevel134, 0, 1, turretSkullLeftFrames
+};
+
+static const int anaDisappearsFrames[] = {
+	 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
+	16, 17, 18, 19, 20, 21, 22, -1
+};
+static const AnimationInfo anaDisappears = {
+	kAnimationAnaDisappears, kFileLevel4, 0, 1, anaDisappearsFrames
+};
+
+static const int anaGlimpseFrames[] = {
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1
+};
+static const AnimationInfo anaGlimpse = {
+	kAnimationAnaGlimpse, kFileLevel2T4, 0, 1, mordamirFireFrames
+};
+
+static const int knifeFrames[] = {
+	 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
+	 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3,
+	-1
+};
+static const AnimationInfo knife = {
+	kAnimationKnife, kFileLevel2T4, 1, 0, mordamirFireFrames
+};
+
+static const int fireball0Frames[] = {
+	31, 32, 33, 32, 34, 35, 36, 35, 37, 38, 39, 38, 40, 41, 42, 41,
+	43, 44, 45, 44, 46, 47, 48, 47, 49, 50, 51, 50, 52, 53, 54, 53,
+	-1
+};
+static const AnimationInfo fireball0 = {
+	kAnimationFireball0, kFileGeneral2, 3, 0, mordamirFireFrames
+};
+
+static const int arrowFrames[] = {
+	55, -1
+};
+static const AnimationInfo arrow = {
+	kAnimationArrow, kFileGeneral2, 3, 0, mordamirFireFrames
+};
+
+static const int miniBallFrames[] = {
+	63, 64, 65, 66, 63, 64, 65, 66, 63, 64, 65, 66, 63, 64, 65, 66,
+	63, 64, 65, 66, 63, 64, 65, 66, 63, 64, 65, 66, 63, 64, 65, 66,
+	-1
+};
+static const AnimationInfo miniBall = {
+	kAnimationMiniBall, kFileGeneral2, 3, 0, mordamirFireFrames
+};
+
+static const int bigSparksFrames[] = {
+	0, 1, 0, -1
+};
+static const AnimationInfo bigSparks = {
+	kAnimationBigSparks, kFileGeneral2, 5, 1, mordamirFireFrames
+};
+
+
+static const int flameFlicker0Frames[] = {
+	0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 2, 1, -1
+};
+static const AnimationInfo flameFlicker0 = {
+	kAnimationFlameFlicker0, kFileGeneral1, 1, 1, mordamirFireFrames
+};
+
+static const int flameFlicker1Frames[] = {
+	0, 0, 1, 2, 13, 14, 15, 16, 4, 2, 3, -1
+};
+static const AnimationInfo flameFlicker1 = {
+	kAnimationFlameFlicker1, kFileGeneral1, 1, 1, mordamirFireFrames
+};
+
+static const int flameFlicker2Frames[] = {
+	0, 1, 2, 3, 20, 21, 22, 23, 24, 25, 26, 27, 5, 4, 3, -1
+};
+static const AnimationInfo flameFlicker2 = {
+	kAnimationFlameFlicker2, kFileGeneral1, 1, 1, mordamirFireFrames
+};
+
+static const int flameNormal0Frames[] = {
+	0, 1, 2, 3, -1
+};
+static const AnimationInfo flameNormal0 = {
+	kAnimationFlameNormal0, kFileGeneral1, 1, 1, mordamirFireFrames
+};
+
+static const int flameNormal1Frames[] = {
+	0, 17, 18, 19, 3, -1
+};
+static const AnimationInfo flameNormal1 = {
+	kAnimationFlameNormal1, kFileGeneral1, 1, 1, mordamirFireFrames
+};
+
+static const int flameNormal2Frames[] = {
+	0, 1, -1
+};
+static const AnimationInfo flameNormal2 = {
+	kAnimationFlameNormal2, kFileGeneral1, 1, 1, mordamirFireFrames
+};
+
+static const int flameOutFrames[] = {
+	28, 28, 28, 28, -1
+};
+static const AnimationInfo flameOut = {
+	kAnimationFlameOut, kFileGeneral1, 1, 1, mordamirFireFrames
+};
+
+static const int flameCandleFlickerFrames[] = {
+	15, 16, 15, 16, 15, 16, 16, -1
+};
+static const AnimationInfo flameCandleFlicker = {
+	kAnimationFlameCandleFlicker, kFileLevel1, 1, 1, mordamirFireFrames
+};
+
+static const int flameCandleLeapFrames[] = {
+	25, 26, 27, 28, 29, 30, 31, -1
+};
+static const AnimationInfo flameCandleLeap = {
+	kAnimationFlameCandleLeap, kFileLevel1, 1, 1, mordamirFireFrames
+};
+
+static const int flameCandleJumpFrames[] = {
+	17, 18, 19, 20, -1
+};
+static const AnimationInfo flameCandleJump = {
+	kAnimationFlameCandleJump, kFileLevel1, 1, 1, mordamirFireFrames
+};
+
+static const int flameCandleSwayFrames[] = {
+	21, 22, 23, 24, -1
+};
+static const AnimationInfo flameCandleSway= {
+	kAnimationFlameCandleSway, kFileLevel1, 1, 1, mordamirFireFrames
+};
+
+static const int flameCandleBurstFrames[] = {
+	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	 0,  0,  0,  0,  0,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
+	11, 12, 13, 14, -1
+};
+static const AnimationInfo flameCandleBurst = {
+	kAnimationFlameCandleBurst, kFileLevel1, 1, 1, mordamirFireFrames
+};
+
+static const int floorTileSinkFrames[] = {
+	0, 1, 2, 3, 3, 3, 3, 4, 5, 6, -1
+};
+static const AnimationInfo floorTileSink = {
+	kAnimationFloorTileSink, kFileLevel1, 0, 1, mordamirFireFrames
+};
+
+static const int norlacDownFrames[] = {
+	0, 1, 2, 3, 4, 5, 6, 7, 8, -1
+};
+static const AnimationInfo norlacDown = {
+	kAnimationNorlacDown, kFileLevel7P1, 1, 1, mordamirFireFrames
 };
 
 }
